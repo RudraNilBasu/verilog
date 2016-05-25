aa4143fa0386c84124ddc7b94ca4ea0747f9b81f
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-05-25 18:20:04 +0530

    basic gate - and

--- Practice/binaryESeg.v
+++ Practice/binaryESeg.v
@@ -8,4 +8,11 @@ module binaryToESeg;
 		g3(p3,~B,~D),
 		g4(p4,A,C),
 		g5(eSeg,p1,p2,p3,p4);
+	initial // Simulator - to propagate the changed values from the outputs of gates and registers to other gate and module inputs.
+	begin
+		#10 A=0; B=0; C=0; D=0;
+		#10 D=1;
+		#10 C=1; D=0;
+		#10 $finish;
+	end
 endmodule
