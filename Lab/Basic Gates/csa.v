f3781a595b1493de65f17f558e37c39f0d4df815
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-05-27 14:58:52 +0530

    Re Doing

--- Lab/Basic Gates/csa.v
+++
@@ -1,8 +0,0 @@
-i am a bad boy .I want to kill sumitro ranodeep saha and samarjit naskar
-keu day?
-na age dito
-K? sagorika?
-na ranodeep gay 
-bujhlam keno sagorika khisti mere break up koreche
-lol^ infility
-
